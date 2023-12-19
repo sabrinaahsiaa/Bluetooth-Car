50c1701e3fbad149f597943a9e1b4554369f5412
Sabrina Hsia <98360662+sabrinaahsiaa@users.noreply.github.com>
2023-12-18 21:40:57 -0800

    Create GPIO.c
    GPIO.c contains the function to initialize Port F

---
+++ GPIO.c
@@ -0,0 +1,15 @@
+#include "tm4c123gh6pm.h"
+#include "GPIO.h"
+
+// Port F Initialization
+void LED_Init(void){ volatile unsigned long delay;
+	SYSCTL_RCGC2_R |= SYSCTL_RCGC2_GPIOF;	// Activate F clocks
+	while ((SYSCTL_RCGC2_R&SYSCTL_RCGC2_GPIOF)==0){};
+		
+  GPIO_PORTF_AMSEL_R &= ~0x0E;      // 3) disable analog function
+  GPIO_PORTF_PCTL_R &= ~0x0000FFF0; // 4) GPIO clear bit PCTL  
+  GPIO_PORTF_DIR_R |= 0x0E;         // 6) PF1-PF3 output
+  GPIO_PORTF_AFSEL_R &= ~0x0E;      // 7) no alternate function     
+  GPIO_PORTF_DEN_R |= 0x0E;         // 8) enable digital pins PF3-PF1
+  LED = Dark;                       // Turn off all LEDs.
+}
